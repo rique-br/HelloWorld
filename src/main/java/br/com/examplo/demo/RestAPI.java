b8184b37951a8918e0744f99bcc424cbc5dabca3
rique-br <rique.br@hotmail.com>
2019-03-22 15:40:50 -0300

    Criação da homepage.

--- src/main/java/br/com/examplo/demo/RestAPI.java
+++ src/main/java/br/com/examplo/demo/RestAPI.java
@@ -1,8 +1,13 @@
 package br.com.examplo.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestAPI {
+	@GetMapping(value = "/")
+	public String getMethodName() {
+		return "Hello World!";
+	}
 
 }
